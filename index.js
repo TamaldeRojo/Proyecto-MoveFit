936b53b17bee2ae6b2ba600a52162c64c3029df9
Josué <josue.medina9991@gmail.com>
2023-05-25 12:09:01 -0600

    second Josue

--- index.js
+++ index.js
@@ -7,6 +7,15 @@ const path = 'C:/Users/josue/Desktop/Proyecto-MoveFit/templates/'
 app.get('/', (req, res) => {
   res.sendFile(path + 'landingPage.html');
 })
+app.get('/about', (req, res) => {
+  res.sendFile(path + 'about.html');
+})
+app.get('/foro', (req, res) => {
+  res.sendFile(path + 'foro.html');
+})
+app.get('/mainMenu', (req, res) => {
+  res.sendFile(path + 'mainMenu.html');
+})
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
