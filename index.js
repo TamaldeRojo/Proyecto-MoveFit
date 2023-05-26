04b7c7d4fc3b82589fa1753ea908f78efb1cab61
Josué <josue.medina9991@gmail.com>
2023-05-25 22:41:53 -0600

    fourth josue

--- index.js
+++ index.js
@@ -8,12 +8,15 @@ const path = __dirname + '/templates/'
 app.get('/', (req, res) => {
   res.sendFile(path + 'landingPage.html');
 })
+
 app.get('/about', (req, res) => {
   res.sendFile(path + 'about.html');
 })
+
 app.get('/foro', (req, res) => {
   res.sendFile(path + 'foro.html');
 })
+
 app.get('/menu', (req, res) => {
   res.sendFile(path + 'mainMenu.html');
 })
